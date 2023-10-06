a400bdbb42bf08759767982dfd95d512f228730b
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2023-10-07 02:46:57 +0900

    [level 1] Title: 삼총사, Time: 0.23 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    
+    for(int i = 0; i < number.size() - 2; i++)
+    {
+        for(int j = i + 1; j < number.size() - 1; j++)
+        {
+            for(int k = j + 1; k < number.size(); k++)
+            {
+                cout << "i: " << number[i] << " " << "j: " << number[j] << " " << "k: " <<number[k] << endl;
+                if(number[i] + number[j] + number[k] == 0)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
