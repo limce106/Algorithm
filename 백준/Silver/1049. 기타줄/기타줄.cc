a5cc5ac2e36037a26ffd0c50b68a42997563c9f3
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2023-10-15 04:28:22 +0900

    Update 기타줄.cc

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -22,9 +22,9 @@ int main() {
         // min_one = min(min_one, y);
     }
 
-    int price1 = (line / 6 + 1) * min_six;
-    int price2 = line / 6 * min_six + line % 6 * min_one;
-    int price3 = line * min_one;
+    int price1 = (line / 6 + 1) * min_six;    // 패키지로만 살 때
+    int price2 = line / 6 * min_six + line % 6 * min_one;    // 패키지, 낱개로 살 때
+    int price3 = line * min_one;    // 낱개로만 살 때
 
     cout << min(price1, min(price2, price3));
 
