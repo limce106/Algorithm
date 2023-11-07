85801cdcda15b8d4288ac4beaac72010a436cb6d
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2023-11-08 01:12:11 +0900

    Update 카드 숫자 곱의 경우의 수.cc

--- 백준/Silver/25370. 카드 숫자 곱의 경우의 수/카드 숫자 곱의 경우의 수.cc
+++ 백준/Silver/25370. 카드 숫자 곱의 경우의 수/카드 숫자 곱의 경우의 수.cc
@@ -11,7 +11,8 @@ void recursion(int count, int multiple){
         arr.push_back(multiple);
         return;
     }
-    
+
+    // 카드 개수만큼 for문 실행
     for(int i = 1; i <= 9; i++){
         recursion(count + 1, multiple * i);
     }
@@ -21,6 +22,8 @@ int main(){
     cin >> n;
 
     recursion(0, 1);
+
+    // 오름차순으로 정렬 후 중복을 제거해야 제거된다. 정렬하지 않고 중복을 제거하려고 하면 제거되지 않는다.
     sort(arr.begin(), arr.end());
     arr.erase(unique(arr.begin(),arr.end()),arr.end());
     cout << arr.size();
