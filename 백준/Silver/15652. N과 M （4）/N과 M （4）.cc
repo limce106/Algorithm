dcefeda185e537db2f9f45c8268456bd70f4e3cb
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2024-08-24 22:57:32 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 9;
+int N, M;
+int arr[MAX];
+
+void dfs(int num, int cnt){
+    if(cnt == M){
+        for(int i = 0; i < M; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+    }
+    else{
+        for(int i = num; i <= N; i++){
+            arr[cnt] = i;
+            dfs(i, cnt + 1);
+        }
+    }
+}
+
+int main(){
+    cin >> N >> M;
+    
+    dfs(1, 0);
+    
+    return 0;
+}
