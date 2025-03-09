4d299d24a038662f15250671b77fef877690934b
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-03-10 02:03:31 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    int dp[1001]; 
+    for(int i = 1; i <= N; i++){
+        cin >> dp[i];
+    }
+    
+    for(int i = 2; i <= N; i++){
+        for(int j = 1; j <= i; j++){
+            dp[i] = min(dp[i], dp[j] + dp[i - j]);
+        }
+    }
+    
+    cout << dp[N] << endl;
+    
+    return 0;
+}
