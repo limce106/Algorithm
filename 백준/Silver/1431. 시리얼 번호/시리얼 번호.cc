8acbab98b1f0081f27c001a09af82f8907f95048
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-03-25 00:51:05 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// O(L)
+int sum(string s) {
+    int total = 0;
+
+    for(int i = 0; i < s.size(); i++){
+        if (s[i] >= '0' && s[i] <= '9') {
+            total += s[i] - '0';
+        }
+    }
+
+    return total;
+}
+
+bool cmp(string a, string b) {
+    int asize = a.size();
+    int bsize = b.size();
+
+    if (asize != bsize)
+        return asize < bsize;
+    
+    if (sum(a) != sum(b))
+        return sum(a) < sum(b);
+
+    return a < b;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    // O(N * L)
+    vector<string> v(n);  
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    // O(N log N) -> 시리얼 번호의 최대 길이가 50이므로 O(50 log 50 * 50)
+    sort(v.begin(), v.end(), cmp);
+
+    // O(N)
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
+
+// N: 기타의 개수 (최대 50)
+// L: 시리얼 번호의 길이 (최대 50)
+// 총 시간 복잡도: O(N log N * L)
+
+// 공간 복잡도
+// sum()이나 cmp()에서 사용하는 임시 변수들은 상수 공간 → O(1)
+// 총 공간 복잡도: O(N * L)
