35124c6e55d5430bbfc97c54fa87389a5e14c736
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-04-12 00:06:51 +0900

    [Silver III] Title: 근손실, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/18429. 근손실/근손실.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n, k;
+vector<int> weight;
+vector<bool> visit;
+int ans = 0;
+
+void solve(int curWeight, int kitCnt){
+    if(kitCnt == n){
+        ans++;
+        return;
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(visit[i] == false){
+            int todayWeight = curWeight - k + weight[i];
+            if(todayWeight < 500){
+                continue;
+            }
+                        
+            visit[i] = true;
+            solve(todayWeight, kitCnt + 1);
+            visit[i] = false;
+        }
+    }
+}
+
+int main(){
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        int input;
+        cin >> input;
+        weight.push_back(input);
+        visit.push_back(false);
+    }
+    
+    solve(500, 0);
+    cout << ans << "\n";
+    
+    return 0;
+}
