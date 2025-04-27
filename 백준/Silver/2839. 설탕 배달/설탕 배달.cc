4810b1e89e3d35cef779345ac67b9c3c27731ede
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-04-27 13:51:10 +0900

    Update 설탕 배달.cc

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -7,13 +7,13 @@ int main()
     int ans = -1;
     cin >> n;
     
-    if(n % 5 == 0)
+    if(n % 5 == 0)    // O(1)
         ans = n / 5;
     else
     {
-        for(int i = n / 5; i >= 0; i--)
+        for(int i = n / 5; i >= 0; i--)    // O(n/5) = O(n)
         {
-            if((n - 5 * i) % 3 == 0)
+            if((n - 5 * i) % 3 == 0)    // O(1)
             {
                 ans = i  + (n - 5 * i) / 3;
                 break;
@@ -25,3 +25,5 @@ int main()
     
     return 0;
 }
+
+// 시간복잡도: O(n)
