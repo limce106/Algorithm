316d7c956aaf8f551ae7c3093c362d0d6e9551d9
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-05-04 18:32:40 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 632 ms, Memory: 8180 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> vn;
+vector<int> vm;
+
+int countByRange(vector<int>& v, int leftValue, int rightValue)
+{
+    vector<int>::iterator rightIndex = upper_bound(v.begin(), v.end(), rightValue);
+    vector<int>::iterator leftIndex = lower_bound(v.begin(), v.end(), leftValue);
+    return rightIndex - leftIndex;
+}
+
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+        vn.push_back(input);
+    }
+    sort(vn.begin(), vn.end());
+    
+    cin >> m;
+    for(int i = 0; i < m; i++)
+    {
+        int input;
+        cin >> input;
+        vm.push_back(input);
+    }
+    
+    for(int i = 0; i < m; i++)
+    {
+        cout << countByRange(vn, vm[i], vm[i]) << ' ';
+    }
+    
+    return 0;
+}
