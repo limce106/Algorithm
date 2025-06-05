f4b612681d79bd3bfb7079a58a6204ace1352d19
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-06-05 19:27:50 +0900

    [level 2] Title: 피로도, Time: 0.88 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -4,32 +4,25 @@
 
 using namespace std;
 
-int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
-    
-    vector<int> v;
+int answer = -1;
+int visit[8];
+
+void dfs(int count, int curK, vector<vector<int>>& dungeons)
+{
     for(int i = 0; i < dungeons.size(); i++)
     {
-        v.push_back(i);
+        if(visit[i] || dungeons[i][0] > curK)
+            continue;
+        
+        visit[i] = true;
+        dfs(count + 1, curK - dungeons[i][1], dungeons);
+        visit[i] = false;
     }
-    
-    int max = 0;
-    do{
-        int temp = 0;
-        int curK = k;
-        for(int i = 0; i < v.size(); i++)
-        {
-            int index = v[i];
-            if(curK >= dungeons[index][0])
-            {
-                curK -= dungeons[index][1];
-                temp++;
-            }
-        }
-        max = max >= temp ? max : temp;
-    }while(next_permutation(v.begin(), v.end()));
-    
-    answer = max;
+    answer = max(answer, count);
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    dfs(0, k, dungeons);
     
     return answer;
 }
