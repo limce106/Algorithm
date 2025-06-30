d012a114b4ddf501105bfc1a06a3b4a07f47d79d
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-07-01 01:37:06 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.23 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -6,71 +6,64 @@ using namespace std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
-vector<vector<bool>> visit;
+
+vector<vector<bool>> visited;
 
 int solution(vector<string> board) {
-    int answer = 0;
     int n = board.size();
     int m = board[0].size();
-    
-    visit.resize(n, vector<bool>(m, false));
+    visited.resize(n, vector<bool>(m, false));
     
     queue<pair<pair<int, int>, int>> q;
-    int find = false;
+    
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
         {
             if(board[i][j] == 'R')
             {
-                q.push({{i, j}, 0});
-                visit[i][j] = true;
-                find = true;
+                q.push({{i, j}, 1});
+                visited[i][j] = true;
                 break;
             }
         }
-        if(find)
-            break;
     }
     
     while(!q.empty())
     {
         int x = q.front().first.first;
         int y = q.front().first.second;
-        int depth = q.front().second;
+        int count = q.front().second;
         q.pop();
         
         for(int i = 0; i < 4; i++)
         {
             int nx = x;
             int ny = y;
-            answer++;
             
             while(true)
-            {
-                int tx = nx + dx[i];
-                int ty = ny + dy[i];
-
-                if(tx < 0 || tx >= n || ty < 0 || ty >= m)
-                    break;
-                if(board[tx][ty] == 'D')
-                    break;
+            {                
+                nx += dx[i];
+                ny += dy[i];
                 
-                nx = tx;
-                ny = ty;
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 'D')
+                {
+                    nx -= dx[i];
+                    ny -= dy[i];
+                    break;
+                }
             }
             
-            if(visit[nx][ny])
-                continue;
             if(board[nx][ny] == 'G')
-            {
-                return depth + 1;
-            }
+                return count;
             
-            q.push({{nx, ny}, depth + 1});
-            visit[nx][ny] = true;
+            if(visited[nx][ny])
+                continue;
+            
+            visited[nx][ny] = true;
+            q.push({{nx, ny}, count + 1});
         }
     }
-
+    
     return -1;
 }
