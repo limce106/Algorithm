3eefd7e8cbf0c21bbcafc9438feb27f264e1c411
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-07-14 23:36:46 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -13,10 +13,12 @@ int solution(vector<vector<string>> clothes) {
         um[clothes[i][1]]++;
     }
     
-    for(auto& it : clothes)
+    for(auto& it : um)
     {
-        answer *= it.value;
+        answer *= (it.second+1);
     }
     
+    // (개수+1)*(개수+1)*.. -> 전체 경우의 수
+    // 모든 의상을 착용하지 않는 경우의 수 제외
     return answer-1;
 }
