6cea3ba9794838ad23db05983bd7ca4492ed040d
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-07-28 01:23:57 +0900

    [level 3] Title: 순위, Time: 1.78 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
-    vector<vector<int>> graph(n+1, vector<int>(n+1,0));
+    int answer = 0;
+    vector<vector<int>> v(n+1, vector<int>(n+1, 0));
     
     for(auto r : results)
     {
         int win = r[0];
         int lose = r[1];
         
-        graph[win][lose] = 1;
-        graph[lose][win] = -1;
+        v[win][lose] = 1;
+        v[lose][win] = -1;
     }
     
     for(int k = 1; k <= n; k++)
@@ -21,31 +22,28 @@ int solution(int n, vector<vector<int>> results) {
         {
             for(int j = 1; j <= n; j++)
             {
-                if(graph[i][k] == 1 && graph[k][j] == 1)
-                    graph[i][j] = 1;
-                else if(graph[i][k] == -1 && graph[k][j] == -1)
-                    graph[i][j] = -1;
+                if(v[i][k] == 1 && v[k][j] == 1)
+                    v[i][j] = 1;
+                else if(v[i][k] == -1 && v[k][j] == -1)
+                    v[i][j] = -1;
             }
         }
     }
     
-    int answer = 0;
     for(int i = 1; i <= n; i++)
-    {
-        int cnt = 0;
+    {        
         for(int j = 1; j <= n; j++)
         {
             if(i == j)
                 continue;
             
-            if(graph[i][j] != 0)
-                cnt++;
+            if(v[i][j] == 0)
+            {
+                answer++;
+                break;
+            }  
         }
-        
-        // 자기 자신을 제외한 나머지 선수들과의 승패를 모두 알고 있다면
-        if(cnt == n - 1)
-            answer++;
     }
     
-    return answer;
+    return n-answer;
 }
