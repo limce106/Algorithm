608c4f802ceea0292779dd17d4fa147552886dba
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-07-29 22:08:09 +0900

    [level 3] Title: 입국심사, Time: 50.95 ms, Memory: 6.78 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -9,26 +10,28 @@ long long solution(int n, vector<int> times) {
     
     sort(times.begin(), times.end());
     
-    long long left = 1;
-    long long right = (long long)times.back() * n;
+    long long start = 1;
+    long long end = (long long)times.back() * n;
     
-    while(left <= right)
+    while(start <= end)
     {
-        long long mid = (left+right)/2;
-        long long cnt = 0;
+        long long mid = (start + end)/2;
         
+        long long people = 0;
         for(int i = 0; i < times.size(); i++)
         {
-            cnt += mid / (long long)times[i];
+            people += (long long)mid/times[i];
         }
         
-        if(cnt >= n)
+        if(people >= n)
         {
             answer = mid;
-            right = mid - 1;   
+            end = mid - 1;
         }
         else
-            left = mid + 1;
+        {
+            start = mid  + 1;
+        }
     }
     
     return answer;
