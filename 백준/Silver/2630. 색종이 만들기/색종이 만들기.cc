27cc0c1a09a6c278cf3f4b4aa72260d34d0ebad3
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-07-31 16:52:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int white = 0, blue = 0;
+int white = 0;
+int blue = 0;
 
-void divide(const vector<vector<int>>& paper, int x, int y, int size)
+void solve(vector<vector<int>>& v, int x, int y, int size)
 {
-    int color = paper[x][y];
     bool same = true;
+    int color = v[x][y];
     
-    for(int i = x; i < x + size; i++)
+    for(int i = x; i < x+size; i++)
     {
-        for(int j = y; j < y + size; j++)
+        for(int j = y; j < y+size; j++)
         {
-            if(paper[i][j] != color)
+            if(color != v[i][j])
             {
                 same = false;
                 break;
@@ -35,10 +35,10 @@ void divide(const vector<vector<int>>& paper, int x, int y, int size)
     else
     {
         int half = size/2;
-        divide(paper, x , y, half);
-        divide(paper, x , y+half, half);
-        divide(paper, x+half , y, half);
-        divide(paper, x+half , y+half, half);
+        solve(v, x, y, half);
+        solve(v, x+half, y, half);
+        solve(v, x, y+half, half);
+        solve(v, x+half, y+half, half);
     }
 }
 
@@ -47,17 +47,16 @@ int main()
     int n;
     cin >> n;
     
-    vector<vector<int>> paper(n,vector<int>(n));
-    
+    vector<vector<int>> v(n, vector<int>(n,0));
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            cin >> paper[i][j];
+            cin >> v[i][j];
         }
     }
     
-    divide(paper, 0, 0, n);
+    solve(v, 0, 0, n);
     
     cout << white << endl;
     cout << blue << endl;
