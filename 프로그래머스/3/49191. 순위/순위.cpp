4785eb1cb654df6abcd63fa39469a8e01956d689
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-08-09 00:05:43 +0900

    [level 3] Title: 순위, Time: 1.83 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
+    
     vector<vector<int>> v(n+1, vector<int>(n+1, 0));
     
-    for(auto r : results)
+    for(auto& r : results)
     {
         int win = r[0];
         int lose = r[1];
@@ -23,26 +24,27 @@ int solution(int n, vector<vector<int>> results) {
             for(int j = 1; j <= n; j++)
             {
                 if(v[i][k] == 1 && v[k][j] == 1)
-                    v[i][j] =  1;
+                    v[i][j] = 1;
                 else if(v[i][k] == -1 && v[k][j] == -1)
-                    v[i][j] =  -1;
+                    v[i][j] = -1;
             }
         }
     }
     
     for(int i = 1; i <= n; i++)
     {
-        int cnt = 0;
+        int count = 0;
         for(int j = 1; j <= n; j++)
         {
             if(i == j)
                 continue;
-            
-            if(v[i][j] != 0)
-                cnt++;
+            if(v[i][j] == 0)
+            {
+                count++;
+            }
         }
         
-        if(cnt == n-1)
+        if(count == 0)
             answer++;
     }
     
