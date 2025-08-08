72802736eb1e2088d10119c067005271930d28e5
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-08-08 23:59:35 +0900

    [Silver I] Title: 절댓값 힙, Time: 556 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+struct cmp{
+    bool operator()(int a, int b)
+    {
+        if(abs(a) == abs(b))
+        {
+            return a > b;
+        }
+        return abs(a) > abs(b);
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    priority_queue<int, vector<int>, cmp> pq;
+    
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        
+        if(x != 0)
+        {
+            pq.push(x);
+        }
+        else
+        {
+            if(pq.empty())
+                cout << 0 << endl;
+            else
+            {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }
+    }
+    
+    return 0;
+}
