a11251cd5ca7601acb8ff5a15c35975f60a7ed8a
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-08-10 00:46:39 +0900

    [Silver II] Title: 숫자판 점프, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.cc
@@ -2,38 +2,37 @@
 #include <set>
 using namespace std;
 
+int arr[5][5];
 int xx[4] = {0, 0, 1, -1};
 int yy[4] = {1, -1, 0, 0};
-int arr[5][5];
-set<int>s; // 중복을 제외하기 위함
+set<int>s;
 
 void DFS(int x, int y, int count, int num){
-    if(count == 5){ // 다섯 번 이동해서 여섯 자리 숫자가 만들어졌을 때
+    if(count == 5){
         s.insert(num);
         return;
     }
     
     for(int i = 0; i < 4; i++){
-        // px, py는 현재 좌표(x, y)에서 이동한 좌표 ([0][0] ~ [4][4])
         int px = x + xx[i];
         int py = y + yy[i];
-           
-        if(px < 0 || py < 0 || px > 4 || py > 4) // 유효한 좌표가 아닐 때
+        
+        if(px < 0 || py < 0 || px > 4 || py > 4)
             continue;
-            
-        DFS(px, py, count + 1, num * 10 + arr[px][py]);
+        
+        DFS(px, py, count + 1, num*10 + arr[px][py]);
     }
 }
 
 int main(){
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
             cin >> arr[i][j];
         }
     }
     
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
             DFS(i, j, 0, arr[i][j]);
         }
     }
