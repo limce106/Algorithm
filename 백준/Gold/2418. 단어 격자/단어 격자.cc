25afd3d48488d67b19a7fb725a30e275e435a4ea
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-08-20 13:41:00 +0900

    [Gold IV] Title: 단어 격자, Time: 4 ms, Memory: 4812 KB -BaekjoonHub

--- 백준/Gold/2418. 단어 격자/단어 격자.cc
+++ 백준/Gold/2418. 단어 격자/단어 격자.cc
@@ -2,41 +2,39 @@
 #include <vector>
 using namespace std;
 
-int dx[8] = {-1,1,0,0,-1,-1,1,1};
-int dy[8] = {0,0,-1,1,-1,1,-1,1};
-
 int h, w, l;
-vector<string> grid;
+vector<string> graph;
 string target;
-long long answer = 0;
-vector<vector<vector<long long>>> dp; // -1: 미방문, 0 이상: 계산된 값
+vector<vector<vector<long long>>> dp;
 
-long long dfs(int x, int y, int count)
-{   
-    if(count == l)
-    {
+long long answer = 0;
+
+int dx[8] ={-1,1,0,0,1,1,-1,-1};
+int dy[8] ={0,0,-1,1,1,-1,1,-1};
+
+long long dfs(int x, int y, int cnt)
+{
+    if(cnt == target.size())
         return 1;
-    }
     
-    // dp[x][y][count]는 (x, y) 위치에서 target의 count번째 문자부터 끝까지 만들 수 있는 경우의 수
-    long long& ret = dp[x][y][count];
-    if(ret != -1)
+    long long& ret = dp[x][y][cnt];
+    if(ret > 0)
         return ret;
     
     ret = 0;
-        
+    
     for(int i = 0; i < 8; i++)
     {
         int nx = x + dx[i];
         int ny = y + dy[i];
         
-        if(nx < 0 || nx >= h || ny < 0 || ny >= w)
+        if(nx < 0 || nx >= h || ny < 0 || ny >=w)
             continue;
         
-        if(target[count] != grid[nx][ny])
-            continue;
-       
-        ret += dfs(nx, ny, count+1);
+        if(graph[nx][ny] == target[cnt])
+        {
+            ret += dfs(nx, ny, cnt+1);
+        }
     }
     
     return ret;
@@ -46,21 +44,21 @@ int main()
 {
     cin >> h >> w >> l;
     
-    grid.resize(h);
+    graph.resize(h);
+    dp.resize(h, vector<vector<long long>>(w, vector<long long>(l, -1)));
+    
     for(int i = 0; i < h; i++)
     {
-        cin >> grid[i];
+        cin >> graph[i];
     }
     
     cin >> target;
-    
-    dp.assign(h, vector<vector<long long>>(w, vector<long long>(l, -1)));
     
     for(int i = 0; i < h; i++)
     {
         for(int j = 0; j < w; j++)
         {
-            if(grid[i][j] == target[0])
+            if(target[0] == graph[i][j])
                 answer += dfs(i, j, 1);
         }
     }
