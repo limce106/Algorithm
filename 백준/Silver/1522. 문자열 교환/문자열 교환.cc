55134d09faef3927511d459469a66e0254e8c79e
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-09-18 00:15:11 +0900

    [Silver I] Title: 문자열 교환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.cc
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.cc
@@ -7,36 +7,43 @@ int main()
     string s;
     cin >> s;
     
-    int  n = s.size();
-    int cntA = count(s.begin(), s.end(), 'a');
-    if(cntA == 0 || cntA == n)
+    int cntA = 0;
+    for(int i = 0; i < s.size(); i++)
     {
-        cout << 0;
+        if(s[i] == 'a')
+            cntA++;
+    }
+    
+    if(cntA == 0 || cntA == s.size())
+    {
+        cout << "0";
         return 0;
     }
     
-    // 문자열을 2배로 이어붙이기. 원형 구간을 표현하기 위함
     s += s;
     
-    int curB = 0;
+    int minCnt = 0;
+    int cntB = 0;
+    
     for(int i = 0; i < cntA; i++)
     {
         if(s[i] == 'b')
-            curB++;
+            cntB++;
     }
     
-    int ans = curB;
+    minCnt = cntB;
+    
     for(int i = cntA; i < s.size(); i++)
-    {
-        if(s[i-cntA] == 'b')
-            curB--; // 윈도우에서 "빠져나가는 문자"가 b라면 b 개수 감소
+    {        
         if(s[i] == 'b')
-            curB++;  // 윈도우에 "새로 들어오는 문자"가 b라면 b 개수 증가
+            cntB++;
+        if(s[i-cntA] == 'b')
+            cntB--;
         
-        ans = min(ans, curB);
+        minCnt = min(minCnt, cntB);
     }
     
-    cout << ans;
+    cout << minCnt;
     
     return 0;
 }
