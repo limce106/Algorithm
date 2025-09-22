fdeb1af8a9794a153c8c41d89688c9fe89402a83
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-09-22 20:38:36 +0900

    [Gold IV] Title: 플로이드, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -3,28 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-const int INF = 1e9;
-
-int n, m;
+const int MAX = 1e9;
 
 int main()
 {
+    int n, m;
     cin >> n;
     cin >> m;
     
-    vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
-    
+    vector<vector<int>> graph(n+1, vector<int>(n+1, MAX));
     for(int i = 1; i <= n; i++)
     {
-        dist[i][i] = 0;
+        graph[i][i] = 0;
     }
+    
+    int a, b, c;
     
     for(int i = 0; i < m; i++)
     {
-        int start, end, cost;
-        cin >> start >> end >> cost;
+        cin >> a >> b >> c;
         
-        dist[start][end] = min(dist[start][end], cost);
+        graph[a][b] = min(graph[a][b], c);
     }
     
     for(int k = 1; k <= n; k++)
@@ -32,10 +31,10 @@ int main()
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= n; j++)
-            {
-                if(dist[i][k] + dist[k][j] < dist[i][j])
+            {  
+                if(graph[i][k] + graph[k][j] < graph[i][j])
                 {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                    graph[i][j] = graph[i][k] + graph[k][j];
                 }
             }
         }
@@ -45,10 +44,10 @@ int main()
     {
         for(int j = 1; j <= n; j++)
         {
-            if(dist[i][j] == INF)
+            if(graph[i][j] == MAX)
                 cout << 0 << " ";
             else
-                cout << dist[i][j] << " ";
+                cout << graph[i][j] << " ";
         }
         cout << endl;
     }
