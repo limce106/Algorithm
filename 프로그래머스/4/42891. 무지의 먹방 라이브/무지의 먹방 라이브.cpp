268a707ec0102aa39343b5d98368a39fd73f290f
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-09-28 21:32:43 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 30.54 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
@@ -27,16 +27,20 @@ int solution(vector<int> food_times, long long k) {
     long long prev_time = 0; // 이전 단계에서 제거한 음식의 시간
     for(int i = 0; i < n; i++)
     {
-        long long cur_time = foods[i].first;
+        long long cur_time = foods[i].first; // 현재 가장 짧은 시간
+        // 먹고 남은 시간
         long long gap = cur_time - prev_time;
         if(gap == 0) continue;
         
+        // 남아있는 음식의 개수만큼 한 바퀴를 돌아서 해당 음식을 0으로 만드는데 필요한 총 시간
         long long spend = gap * (n - i);
+        // 현재 처리 중인 음식을 다 먹을 수 있는 경우
         if(spend <= k)
         {
             k -= spend;
-            prev_time = cur_time;
+            prev_time = cur_time; // 이번에 다 먹은 음식의 시간을 기록
         }
+        // 현재 음식을 다 먹기 전에 네트워크 장애 발생
         else
         {
             vector<pair<int, int>> remain;
