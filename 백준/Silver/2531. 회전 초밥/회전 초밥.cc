b67668bfcec865417e252759c38ac35415a0cad0
Chaeeun Lim <94890726+limce106@users.noreply.github.com>
2025-10-04 01:09:26 +0900

    [Silver I] Title: 회전 초밥, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -7,40 +7,42 @@ int main()
     int n, d, k, c;
     cin >> n >> d >> k >> c;
     
-    vector<int> sushi(n);    
+    vector<int> sushi(n);
     for(int i = 0; i < n; i++)
     {
         cin >> sushi[i];
     }
     
-    vector<int> cnt(d + 1, 0); // 각 종류별 개수
-    int uniqueCnt = 0;    // 중복되지 않는 초밥 종류 개수
+    int uniqueCnt = 0;
+    int eat[3001] = {0,};
+    int ans = 0;
     
-    // 초기 윈도우 [0, k-1]
-    for (int i = 0; i < k; i++) {
-        if (cnt[sushi[i]] == 0) 
+    for(int i = 0; i < k; i++)
+    {
+        if(eat[sushi[i]] == 0)
+        {
             uniqueCnt++;
-        
-        cnt[sushi[i]]++;
+        }
+        eat[sushi[i]]++;
     }
     
-    int ans = uniqueCnt + (cnt[c] == 0 ? 1 : 0);
+    ans = uniqueCnt + (eat[c] == 0 ? 1 : 0);
     
-    for (int i = 1; i < n; i++) 
+    for(int i = 1; i < n; i++)
     {
-        int left = sushi[i - 1];
-        cnt[left]--;
+        int next = sushi[(i + k - 1) % n];
+        int before = sushi[i - 1];
         
-        if (cnt[left] == 0) 
-            uniqueCnt--;
-        
-        int right = sushi[(i + k - 1) % n];
-        if (cnt[right] == 0)
+        if(eat[next] == 0)
             uniqueCnt++;
         
-        cnt[right]++;
+        eat[next]++;
+        eat[before]--;
         
-        ans = max(ans, uniqueCnt + (cnt[c] == 0 ? 1 : 0));
+        if(eat[before] == 0)
+            uniqueCnt--;
+        
+        ans = max(ans, uniqueCnt + (eat[c] == 0 ? 1 : 0));
     }
     
     cout << ans;
